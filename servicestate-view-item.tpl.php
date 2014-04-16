6ca9a8b6b66fb47905841d64f0e25107aacc785b
Edward Murrell <edward@catalyst-au.net>
2014-04-16 15:30:32 +1000

    Build a table filled programatically and updatable via ajax.

--- servicestate-view-item.tpl.php
+++ servicestate-view-item.tpl.php
@@ -1,6 +1,8 @@
-<div>
-  <?php print $title; ?>
-</div>
-<div>
-  <?php print $state; ?>
+<div class="<?php print implode(' ', $variables['classes_array']); ?>" id="<?php print SERVICESTATE_ID_PREFIX.'-'.$service; ?>">
+  <div>
+    <?php print $title; ?>
+  </div>
+  <div>
+    <?php print $state; ?>
+  </div>
 </div>
